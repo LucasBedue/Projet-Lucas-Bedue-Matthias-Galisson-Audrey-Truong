c006d14a0eccc4d39e1623bedeb56f7eeaec7c62
Bedue Lucas <beduelucas@pc-cy310-037.cpi.cergy.eisti.fr>
2023-05-09 11:37:26 +0200

    ajout de boutons au menu

--- paflechien/menu.php
+++ paflechien/menu.php
@@ -14,6 +14,18 @@
 
         if ($role=='Jeune'){
             echo "<p>Salut à toi, jeune entrepreneur !</p>";
+            echo "<form method='post' action='listeexperience.php'>
+                    <input type='submit' value='Voir toutes les expèriences'>
+                </form>";
+            echo "<form method='post' action='creerexperience.php'>
+                <input type='submit' value='Créer une expèrience'>
+            </form>";
+            echo "<form method='post' action='avoirunlienconsultant.php'>
+                <input type='submit' value='Obtenez un lien a envoyer à un consultant'>
+            </form>";
+            echo "<form method='post' action='experienceCV.php'>
+                <input type='submit' value='Visionnez vos expèriences au format pdf'>
+            </form>";
             echo "<form method='post' action='logout.php'>
                     <input type='submit' value='Déconnexion'>
                 </form>";
