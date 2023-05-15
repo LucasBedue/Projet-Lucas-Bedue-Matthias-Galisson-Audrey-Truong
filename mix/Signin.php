6b464806edf1a33c1f5cd1ea28e946e92db60fe9
Lastbaguette <114499223+Lastbaguette@users.noreply.github.com>
2023-05-15 10:30:37 +0200

    mb

---
+++ mix/Signin.php
@@ -0,0 +1,44 @@
+<?php
+
+$nom = $_POST['nom'];
+$prenom = $_POST['prenom'];
+$mail = $_POST['mail'];
+$date_naissance = $_POST['date_naissance'];
+$mdp = $_POST['mdp'];
+$role = $_POST['role'];
+
+if (strpos($mail, '@') === false) {
+    echo "<p>L'adresse e-mail n'est pas valide.</p>";
+    exit;
+}
+if (strpos($mdp, ';') !== false) {
+    
+    echo "<p>Le mot de passe ne peut pas contenir le caractère ';'.</p>";
+    exit;
+}
+
+$fichier = fopen('infopers.txt', 'r+');
+
+
+while(!feof($fichier)) {
+    $line = fgets($fichier);
+    $values = explode(";", $line);
+    $mail_fichier = isset($values[3]) ? trim($values[3]) : null;
+
+    if ($mail_fichier==$mail) {
+        fclose($fichier);
+        echo "<p>Cet adresse mail est deja utilisé pour un compte existant.</p>";  
+        exit;
+    }
+}
+
+$fichier = fopen('infopers.txt', 'a');
+
+fwrite($fichier, $nom . ';' . $prenom . ';' . $date_naissance . ';' . $mail . ';' . $mdp . ';' . $role . ";\n");
+fclose($fichier);
+
+header('Location: login.php');
+
+?>
+
+
