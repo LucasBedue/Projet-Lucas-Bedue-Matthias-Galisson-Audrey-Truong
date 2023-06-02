548f7612d129e45b156c6c135c175f0336ad5094
Audrey Truong <crystale.trg@outlook.com>
2023-06-02 15:06:26 +0200

    css not working in reception ?

--- mix/receptionpage.php
+++ mix/receptionpage.php
@@ -6,34 +6,33 @@
 	<body>
 		<link rel="stylesheet" href="receptionpage.css" />
 
-			<div class="bg">
-				<div class="toptext">
-					<p>
-						Pour faire de l'engagement <br />
-						une valeur !
-					</p>
-				</div>
-				<img class="imgpos" src="logos/logo1.png" alt="Jeunes 6.4" />
-				<div class="bottomtext">
-					<p>
-						... l'expression d'un potentiel, <br />
-						la promesse d'une richesse !
-					</p>
-				</div>
-				<div class="enter">
-					<p>ENTRER</p>
-				</div>
-			</div>
-			<div class="bottombanner">
+		<div class="bg">
+			<div class="toptext">
 				<p>
-					JEUNES 6.4 est un dispositif de valorisation de l'engagement
-					des jeunes en Pyrénées-Atlantiques soutenu par l'Etat,
-					<br />
-					le Conseil général, le conseil régional, les CAF Béarn-Soule
-					et Pays Basque, la MSA, <br />
-					l'université de Pau et des pays de l'Adour, la CPAM.
+					Pour faire de l'engagement <br />
+					une valeur !
 				</p>
 			</div>
-
+			<img class="imgpos" src="logos/logo1.png" alt="Jeunes 6.4" />
+			<div class="bottomtext">
+				<p>
+					... l'expression d'un potentiel, <br />
+					la promesse d'une richesse !
+				</p>
+			</div>
+			<div class="enter">
+				<a class="enterlink" href="infopage.php">ENTRER</a>
+			</div>
+		</div>
+		<div class="bottombanner">
+			<p>
+				JEUNES 6.4 est un dispositif de valorisation de l'engagement des
+				jeunes en Pyrénées-Atlantiques soutenu par l'Etat,
+				<br />
+				le Conseil général, le conseil régional, les CAF Béarn-Soule et
+				Pays Basque, la MSA, <br />
+				l'université de Pau et des pays de l'Adour, la CPAM.
+			</p>
+		</div>
 	</body>
 </html>
